f22ad1c82a4f9cb11d94a8d1f572566bb94e2b0c
Cristian Fabbri <cristian.fabbri6@studio.unibo.it>
2021-04-11 11:43:05 +0200

    anonymous game tested, OK

--- app/src/main/java/com/example/thepwnedgame/eventdispatcher/EventDispatcherImpl.java
+++ app/src/main/java/com/example/thepwnedgame/eventdispatcher/EventDispatcherImpl.java
@@ -2,12 +2,14 @@ package com.example.thepwnedgame.eventdispatcher;
 
 import android.content.Context;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Vibrator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.thepwnedgame.GameActivity;
 import com.example.thepwnedgame.GameOverActivity;
+import com.example.thepwnedgame.R;
 import com.example.thepwnedgame.socketevents.GuessEvent;
 import com.example.thepwnedgame.socketevents.SocketEvent;
 import com.example.thepwnedgame.socketevents.SocketGuessEvent;
@@ -23,6 +25,8 @@ public class EventDispatcherImpl implements EventDispatcher{
 
     private BlockingQueue<SocketEvent> queue;
 
+    private static final String correctAnswer = "raw/correct_answer.wav";
+
     public EventDispatcherImpl() {
         this.queue = new LinkedBlockingQueue<>();
     }
@@ -33,16 +37,17 @@ public class EventDispatcherImpl implements EventDispatcher{
         final String eventName = event.getName();
         GameActivity gameActivity = (GameActivity) activity;
         if (eventName.equals("on-error") || eventName.equals("game-end")){
-            //TODO: to be tested
             final Vibrator vibe = (Vibrator) activity.getSystemService(Context.VIBRATOR_SERVICE);
             Intent gameOverIntent = new Intent(gameActivity, GameOverActivity.class);
             gameActivity.getSocket().disconnect();
-            vibe.vibrate(200);
+            vibe.vibrate(50);
             gameActivity.startActivity(gameOverIntent);
 
         }
         if (eventName.equals("guess")){
+            final MediaPlayer correctAnswer = MediaPlayer.create(activity, R.raw.correct_answer);
             GuessEvent guessEvent = new SocketGuessEvent(event);
+            correctAnswer.start();
             passwordViewModel.setFirstPassword(guessEvent.getFirstPassword());
             passwordViewModel.setSecondPassword(guessEvent.getSecondPassword());
             passwordViewModel.setValue(guessEvent.getFirstValue());
